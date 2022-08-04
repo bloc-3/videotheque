f679bd5c262bf065fe1d6df2cf1e107f9e6d11ea
flashjaysan <jaysan@wanadoo.fr>
2022-08-04 08:21:48 +0200

    Update Comments.js

--- src/components/detailsfilms/Comments.js
+++ src/components/detailsfilms/Comments.js
@@ -1,4 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
+
+import { collection, getDocs, query, where } from "firebase/firestore";
+
+import { context } from "../../context";
 
 import Comment from "./Comment";
 import CommentForm from "./CommentForm";
@@ -6,8 +10,48 @@ import CommentForm from "./CommentForm";
 const Comments = ({ movieId }) => {
   const [comments, setComments] = useState([]);
 
-  const nickname = "flash";
+  const { db } = useContext(context);
 
+  let nickname = "none";
+
+  useEffect(() => {
+    const getCommentsFromFirestore = async () => {
+      const commentsCollection = collection(db, "comments");
+      const q = query(commentsCollection, where("movie_id", "==", movieId));
+      const commentsSnapshots = await getDocs(q);
+
+      commentsSnapshots.forEach(async (doc) => {
+
+        // const getNicknameFromFirestore = async (userId) => {
+        //   const usersCollection = collection(db, "users");
+        //   const q = query(usersCollection, where("user_id", "==", userId));
+        //   const usersSnapshots = await getDocs(q);
+
+        //   usersSnapshots.forEach((doc) => {
+        //     return doc.data().nickname;
+        //   });
+        // }
+        const getNicknameFromFirestore = async (userId) => {
+          const usersCollection = collection(db, "users");
+          const q = query(usersCollection, where("user_id", "==", userId));
+          const usersSnapshots = await getDocs(q);
+          return usersSnapshots.docs[0].data().nickname;
+        }
+
+        const dataWithId = {id: doc.id, ...doc.data()};
+        const userId = dataWithId.user_id;
+        const nickname = await getNicknameFromFirestore(userId);
+
+        setComments((previous) => {
+          const fullData = {nickname: nickname, ...dataWithId};
+          console.log(fullData);
+          return [...previous, fullData];
+        });
+      });
+    };
+
+    getCommentsFromFirestore();
+  }, []);
 
   return (
     <div className="comments-section">
@@ -15,8 +59,8 @@ const Comments = ({ movieId }) => {
       <ul>
         {comments.map((comment) => {
           return (
-            <li key={Math.random()} className="comment">
-              <Comment nickname={nickname} comment={"vide"} />
+            <li key={comment.id} className="comment">
+              <Comment nickname={nickname} comment={comment.comment} />
             </li>
           );
         })}
